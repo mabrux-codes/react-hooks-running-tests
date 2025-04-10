c62695173e64e8cefb261494bfbc3897305d290e
Abel Lukwaro <abelmabruke@gmail.com>
2025-04-10 12:34:49 +0300

    Initial Commit

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,9 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  screen.debug();
+  // The test will fail if the text is not found
+  
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
